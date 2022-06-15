b56043d02b58d38ffa1eb05f12995bed5dd3f3b7
jodhernandezbe <jodhernandezbemj@gmail.com>
2022-06-15 15:28:03 -0500

    essay commit

---
+++ app/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Importing libraries
+from fastapi import FastAPI, Request, Query, Path
+import os
+import time
+from fastapi.templating import Jinja2Templates
+import jwt
+
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) # current directory path
+
+templates_path = f'{dir_path}/templates'
+templates = Jinja2Templates(directory=templates_path)
+
+METABASE_SITE_URL = "http://analitica.globalwork.co"
+METABASE_SECRET_KEY = "17b1325e3daf5c3234e726219873ce67e2c3684f54a126fbddd2eb83ea801958"
+
+app = FastAPI()
+
+def get_token(payload):
+    return jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+
+
+@app.get('/clients/{client_id}')
+def get_dashboard(request: Request,
+                client_id: int = Path(title="The client ID"),
+                month_date :str = Query(title="Query month", regex="^[0-9]{4}-[0-9]{2}$")):
+
+  payload = {
+  "resource": {"dashboard": 30},
+  "params": {
+    "id_del_cliente": client_id,
+    "mes": month_date
+  },
+  "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+    }
+
+
+  iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + get_token(payload) + "#bordered=false&titled=false"
+  print(iframeUrl)
+  return templates.TemplateResponse("base.html", {"request": request,
+                                                  "iframeUrl": iframeUrl})
